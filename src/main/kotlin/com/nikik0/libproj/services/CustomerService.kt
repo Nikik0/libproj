506f20bf9c0fa12ba5ec9736e3257365e90a35ad
Nikita <nikitapolin47@gmail.com>
2023-09-15 15:54:38 +1000

    added lists for watched and fav movies

--- src/main/kotlin/com/nikik0/libproj/services/CustomerService.kt
+++ src/main/kotlin/com/nikik0/libproj/services/CustomerService.kt
@@ -1,6 +1,7 @@
 package com.nikik0.libproj.services
 
 import com.nikik0.libproj.dtos.CustomerDto
+import com.nikik0.libproj.dtos.FilmDto
 import com.nikik0.libproj.entities.AddressEntity
 import com.nikik0.libproj.entities.CustomerEntity
 import com.nikik0.libproj.mappers.mapToAddress
@@ -8,29 +9,59 @@ import com.nikik0.libproj.mappers.mapToCustomerEntityAndAddress
 import com.nikik0.libproj.mappers.toDto
 import com.nikik0.libproj.repositories.AddressRepository
 import com.nikik0.libproj.repositories.CustomerRepository
+import com.nikik0.libproj.repositories.FilmRepository
 import kotlinx.coroutines.flow.map
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService (
     private val customerRepository: CustomerRepository,
-    private val addressRepository: AddressRepository
+    private val addressRepository: AddressRepository,
+    private val filmRepository: FilmRepository
         ){
     suspend fun getCustomer(id: Long) = customerRepository.findById(id)?.toDto()
 
     suspend fun getAllCustomers() = customerRepository.findAll().map { it.toDto() }
 
-    suspend fun saveCustomer(customer: CustomerDto) : CustomerDto {
+    suspend fun saveCustomer(customer: CustomerDto): CustomerDto {
         val address = customer.mapToAddress()
-        val customerToSave = CustomerEntity(
-            id = customer.id,
-            name = customer.name,
-            surname = customer.surname,
-            address = addressRepository.save(address)
-        )
-        return customerRepository.save(customerToSave).toDto()
+        return customerRepository.findById(customer.id)
+            ?.let {
+                customerRepository.save(
+                    CustomerEntity(
+                        id = it.id,
+                        name = customer.name,
+                        surname = customer.surname,
+                        address = addressRepository.save(address),
+                        watched = it.watched,
+                        favorites = it.favorites
+                    )
+                ).toDto()
+            }
+            ?: customerRepository.save(
+                CustomerEntity(
+                    id = customer.id,
+                    name = customer.name,
+                    surname = customer.surname,
+                    address = addressRepository.save(address),
+                    watched = emptyList(),
+                    favorites = emptyList()
+                )
+            ).toDto()
     }
 
     suspend fun deleteCustomer(customer: CustomerDto) =
         customerRepository.deleteById(customer.id)
+
+    suspend fun addToWatched(customer: CustomerDto, filmDto: FilmDto) {
+        val film = filmRepository.findById(filmDto.id)
+        customerRepository.findById(customer.id)?:
+        customerRepository.save(
+            CustomerEntity(
+                id = .id,
+
+            )
+        )
+
+    }
 }
