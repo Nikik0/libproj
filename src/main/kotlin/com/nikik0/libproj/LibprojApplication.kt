21f0031bbe64bb66a6a6199660b5906ccd9e4809
Nikita <nikitapolin47@gmail.com>
2023-09-17 17:47:08 +1000

    base classes

---
+++ src/main/kotlin/com/nikik0/libproj/LibprojApplication.kt
@@ -0,0 +1,11 @@
+package com.nikik0.libproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibprojApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibprojApplication>(*args)
+}
---
+++ src/test/kotlin/com/nikik0/libproj/LibprojApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nikik0.libproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibprojApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
