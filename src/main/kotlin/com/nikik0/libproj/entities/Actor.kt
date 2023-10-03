41d28781c5dad7978c87c483b8599ceafddd9f32
Nikita <nikitapolin47@gmail.com>
2023-10-03 15:12:46 +1000

    added id annotation

--- src/main/kotlin/com/nikik0/libproj/entities/Actor.kt
+++ src/main/kotlin/com/nikik0/libproj/entities/Actor.kt
@@ -1,9 +1,11 @@
 package com.nikik0.libproj.entities
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("actor")
 data class Actor(
+    @Id
     val id: Long,
     val name: String,
     val surname: String,
--- src/main/kotlin/com/nikik0/libproj/entities/MovieTag.kt
+++ src/main/kotlin/com/nikik0/libproj/entities/MovieTag.kt
@@ -1,9 +1,11 @@
 package com.nikik0.libproj.entities
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("tag")
 data class MovieTag(
+    @Id
     val id: Long,
     val name: String
 )
