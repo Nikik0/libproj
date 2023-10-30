c3908c68c3247ff2e0989a68e3f07109f2332503
Nikita <nikitapolin47@gmail.com>
2023-10-30 16:25:39 +1000

    mapping moved to entity and dto classes for customer

--- src/main/kotlin/com/nikik0/libproj/dtos/CustomerDto.kt
+++ src/main/kotlin/com/nikik0/libproj/dtos/CustomerDto.kt
@@ -1,5 +1,43 @@
 package com.nikik0.libproj.dtos
 
+import com.nikik0.libproj.entities.AddressEntity
+import com.nikik0.libproj.entities.CustomerEntity
+
+fun CustomerDto.mapToAddress() =
+    AddressEntity(
+        id = this.id,
+        country = this.country,
+        state = this.state,
+        city = this.city,
+        district = this.district,
+        street = this.street,
+        building = this.building!!,
+        buildingLiteral = this.buildingLiteral,
+        apartmentNumber = this.apartmentNumber,
+        additionalInfo = this.additionalInfo
+    )
+
+fun CustomerDto.mapToCustomerEntityAndAddress(): Pair<CustomerEntity, AddressEntity> {
+    val address = AddressEntity(            //todo refactor for null checks
+        id = this.id,
+        country = this.country!!,
+        state = this.state!!,
+        city = this.city!!,
+        district = this.district!!,
+        street = this.street!!,
+        building = this.building!!,
+        buildingLiteral = this.buildingLiteral!!,
+        apartmentNumber = this.apartmentNumber!!,
+        additionalInfo = this.additionalInfo!!
+    )
+    val customerEntity = CustomerEntity(
+        id = this.id,
+        name = this.name,
+        surname = this.surname
+    )
+    return Pair(customerEntity, address)
+}
+
 data class CustomerDto(
     val id: Long,
     val name: String,
