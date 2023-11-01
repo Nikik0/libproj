d72c995b48cfa09180acafd3938d9eed1c592c65
Nikita <nikitapolin47@gmail.com>
2023-11-01 21:51:46 +1000

    added searching movies by tag

--- src/main/kotlin/com/nikik0/libproj/repositories/MovieRepository.kt
+++ src/main/kotlin/com/nikik0/libproj/repositories/MovieRepository.kt
@@ -1,13 +1,16 @@
 package com.nikik0.libproj.repositories
 
 import com.nikik0.libproj.entities.MovieEntity
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
+
 @Repository
 interface MovieRepository: CoroutineCrudRepository<MovieEntity,Long> {
 
+    @Query("select * from movie m join tag_movie tm on m.id = tm.movie_id where tm.tag_id = :tagId")
+    fun findMoviesByTagId(tagId: Long): Flow<MovieEntity>
 
- //   fun getSingle(id: Long): MovieEntity
 }
