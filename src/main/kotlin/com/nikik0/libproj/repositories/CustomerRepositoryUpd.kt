8e0ab51ab33321b71269817daa666d864cf9b25c
Nikita <nikitapolin47@gmail.com>
2023-11-03 18:06:43 +1000

    removed unused repos

--- src/main/kotlin/com/nikik0/libproj/repositories/CustomerRepositoryUpd.kt
+++
@@ -1,9 +0,0 @@
-package com.nikik0.libproj.repositories
-
-import com.nikik0.libproj.entities.CustomerEntityUpd
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CustomerRepositoryUpd: CoroutineCrudRepository<CustomerEntityUpd, Long> {
-}
