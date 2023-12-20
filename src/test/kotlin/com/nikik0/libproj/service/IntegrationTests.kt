ec2a3f9b91565f44e9380610608ece0e29054ebe
Nikita <nikitapolin47@gmail.com>
2023-12-20 15:26:10 +1000

    refactored all integration tests to remove dependency on test order

--- src/test/kotlin/com/nikik0/libproj/service/IntegrationTests.kt
+++ src/test/kotlin/com/nikik0/libproj/service/IntegrationTests.kt
@@ -77,7 +77,9 @@ class IntegrationTests(
         }
     }
 
-    private var customerSavedDummy: CustomerDto? = null
+    private var customerSavedFirstDummy: CustomerDto? = null
+
+    private var customerSavedSecondDummy: CustomerDto? = null
 
     private val customerUnsavedFirstDummy: String = """
         {
@@ -193,46 +195,29 @@ class IntegrationTests(
         "studio", "studio_movie", "tag", "tag_movie"
     )
 
-    private val sequences = listOf(
-        "actor_id_seq", "actor_id_seq", "customer_id_seq",
-        "movie_id_seq", "studio_id_seq", "tag_id_seq"
-    )
-
     @AfterEach
     fun cleanDb(){
-        println("AFTER EACH STARTED")
-
-        val retrievedListOfMovies = webClient.get().uri("/api/v1/movie/get/all/yeager")
-            .exchange().expectBodyList(MovieDto::class.java)
-            .returnResult()
-
-        println("before cleanup total movies are $retrievedListOfMovies")
-
         runBlocking {
-            println("before truncate")
-            for (seq in sequences)
-                client.sql("select * from $seq").map { row -> row.get("last_value") }.all().subscribe { th -> println(th) }
-
             for (table in tables)
                 client.sql("truncate $table restart identity cascade ").await()
 
-            println(" after truncate")
-            for (seq in sequences)
-                client.sql("select * from $seq").map { row -> row.get("last_value") }.all().subscribe { th -> println(th) }
         }
-
-        val retrievedListOfMovies2 = webClient.get().uri("/api/v1/movie/get/all/yeager")
-            .exchange().expectBodyList(MovieDto::class.java)
-            .returnResult()
-
-        println("after cleanup total movies are $retrievedListOfMovies2")
-
-
-        println("AFTER EACH FINISHED")
     }
 
     fun setupTestCustomer(){
-        val address = AddressEntity(
+        val addressSecond = AddressEntity(
+            id = 2,
+            country = "Second country",
+            state = "Second state",
+            city = "Second city",
+            district = "Second district",
+            street = "Second street",
+            building = 1,
+            buildingLiteral = "Second literal",
+            apartmentNumber = 23,
+            additionalInfo = "Second additional info"
+        )
+        val addressFirst = AddressEntity(
             id = 1,
             country = "First country",
             state = "First state",
@@ -244,48 +229,25 @@ class IntegrationTests(
             apartmentNumber = 23,
             additionalInfo = "First additional info"
         )
-        customerSavedDummy = CustomerEntity(
+        customerSavedFirstDummy = CustomerEntity(
             id = 1,
             name = "First name",
             surname = "First Surname",
-            address = address,
+            address = addressFirst,
+            watched = emptyList(),
+            favorites = emptyList()
+        ).toDto()
+        customerSavedSecondDummy = CustomerEntity(
+            id = 2,
+            name = "Second name",
+            surname = "Second Surname",
+            address = addressSecond,
             watched = emptyList(),
             favorites = emptyList()
         ).toDto()
     }
 
     fun setupTestMovie() {
-//        {
-//            "name": "Second Test Movie",
-//            "producer": "Second Test Producer",
-//            "actors": [
-//            {
-//                "name": "Third Actor name",
-//                "surname": "Third Actor surname",
-//                "age": 20
-//            },
-//            {
-//                "name": "Forth Actor name",
-//                "surname": "Forth Actor surname",
-//                "age": 30
-//            }
-//            ],
-//            "tags": [
-//            {
-//                "name": "Mystic"
-//            },
-//            {
-//                "name": "Action"
-//            }
-//            ],
-//            "studio": {
-//            "name": "Second Test Studio",
-//            "employees": 2000,
-//            "owner": "Second Studio Owner"
-//        },
-//            "budget": 2000000,
-//            "movieUrl": "someurl.com/url2"
-//        }
         val actorOne = Actor(
             id = 1L,
             name = "First Actor name",
@@ -358,15 +320,22 @@ class IntegrationTests(
 
     fun insertTestData(){
         println("BEFORE EACH STARTED")
+
+        //first movie insert
         webClient.post().uri("/api/v1/movie/save").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
             .bodyValue(movieUnsavedFirstDummy).exchange().returnResult(MovieDto::class.java).responseBody.blockLast()
+
+        //first customer insert
+        webClient.post().uri("/api/v1/customer/save")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+            .bodyValue(customerUnsavedFirstDummy).exchange().returnResult<CustomerDto>().responseBody.blockLast()
         println("BEFORE EACH FINISHED")
     }
 
     @BeforeEach
     fun setupBeforeTests(){
-        setupTestCustomer()
         setupTestMovie()
+        setupTestCustomer()
         insertTestData()
     }
 
@@ -384,7 +353,7 @@ class IntegrationTests(
 
     @Test
     fun `get single should return correct movieDto`() {
-        val retrievedEntity = webClient.get().uri("/api/v1/movie/get/1").exchange().returnResult<MovieDto>()
+        val retrievedEntity = webClient.get().uri("/api/v1/movie/get/${movieSavedFirstDummy?.id}").exchange().returnResult<MovieDto>()
             .responseBody.blockLast()
         assertThat(retrievedEntity).isEqualTo(movieSavedFirstDummy)
     }
@@ -435,63 +404,60 @@ class IntegrationTests(
             .returnResult()
     }
 
-//    @Test
-//    @Order(6)
-//    fun `save customer returns correct customer dto`(){
-//        val customerDto = webClient.post().uri("/api/v1/customer/save")
-//            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
-//            .bodyValue(customerUnsavedFirstDummy).exchange().returnResult<CustomerDto>().responseBody.blockLast()
-//        assertThat(customerDto).isEqualTo(customerSavedDummy)
-//        webClient.post().uri("/api/v1/customer/save")
-//            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
-//            .bodyValue(customerUnsavedSecondDummy).exchange().returnResult<CustomerDto>()
-//    }
-//
-//    @Test
-//    @Order(7)
-//    fun `get customer returns single customerDto`(){
-//        val customerDto = webClient.get().uri("/api/v1/customer/get/1").exchange()
-//            .returnResult<CustomerDto>().responseBody.blockLast()
-//        assertThat(customerDto).isEqualTo(customerSavedDummy)
-//    }
-//
-//    @Test
-//    @Order(8)
-//    fun `get all customers returns correct number of dtos`(){
-//        webClient.get().uri("/api/v1/customer/get/all").exchange()
-//            .expectBodyList(CustomerDto::class.java).hasSize(2)
-//            .returnResult().responseBody
-//    }
-//
-//    @Test
-//    @Order(9)
-//    fun `add to watched returns dto with correct watched list`(){
-//        val customer = webClient.get().uri("/api/v1/customer/get/1").exchange()
-//            .returnResult<CustomerDto>().responseBody.blockLast()
-//        val movie = webClient.get().uri("/api/v1/movie/get/${movieSavedDummy?.id}").exchange()
-//            .returnResult<MovieDto>().responseBody.blockLast()
-//        val customerUpdated = webClient.post().uri("/api/v1/customer/${customer!!.id}/watched/add")
-//            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
-//            .bodyValue(movieSavedDummy!!).exchange()
-//            .returnResult<CustomerDto>().responseBody.blockLast()
-//        assertThat(customerUpdated!!.watched).isNotNull.isNotEmpty.hasSize(1)
-//        assertThat(customerUpdated.watched!![0].id).isEqualTo(movie!!.id)
-//    }
-//
-//
-//    @Test
-//    @Order(10)
-//    fun `add to watched returns dto with correct favourites list`(){
-//        val customer = webClient.get().uri("/api/v1/customer/get/1").exchange()
-//            .returnResult<CustomerDto>().responseBody.blockLast()
-//        val movie = webClient.get().uri("/api/v1/movie/get/${movieSavedDummy?.id}").exchange()
-//            .returnResult<MovieDto>().responseBody.blockLast()
-//        val customerUpdated = webClient.post().uri("/api/v1/customer/${customer!!.id}/favourites/add")
-//            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
-//            .bodyValue(movieSavedDummy!!).exchange()
-//            .returnResult<CustomerDto>().responseBody.blockLast()
-//        assertThat(customerUpdated!!.favourites).isNotNull.isNotEmpty.hasSize(1)
-//        assertThat(customerUpdated.favourites!![0].id).isEqualTo(movie!!.id)
-//    }
+    @Test
+    fun `save customer returns correct customer dto`(){
+        val customerDto = webClient.post().uri("/api/v1/customer/save")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+            .bodyValue(customerUnsavedSecondDummy).exchange().returnResult<CustomerDto>().responseBody.blockLast()
+        assertThat(customerDto).isEqualTo(customerSavedSecondDummy)
+    }
+
+    @Test
+    fun `get customer returns single customerDto`(){
+        val customerDto = webClient.get().uri("/api/v1/customer/get/${customerSavedFirstDummy?.id}").exchange()
+            .returnResult<CustomerDto>().responseBody.blockLast()
+        assertThat(customerDto).isEqualTo(customerSavedFirstDummy)
+    }
+
+    @Test
+    fun `get all customers returns correct number of dtos`(){
+        webClient.get().uri("/api/v1/customer/get/all").exchange()
+            .expectBodyList(CustomerDto::class.java).hasSize(1)
+            .returnResult().responseBody
+        webClient.post().uri("/api/v1/customer/save")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+            .bodyValue(customerUnsavedSecondDummy).exchange().returnResult<CustomerDto>()
+        webClient.get().uri("/api/v1/customer/get/all").exchange()
+            .expectBodyList(CustomerDto::class.java).hasSize(2)
+            .returnResult().responseBody
+    }
+
+    @Test
+    fun `add to watched returns dto with correct watched list`(){
+        val customer = webClient.get().uri("/api/v1/customer/get/${movieSavedFirstDummy?.id}").exchange()
+            .returnResult<CustomerDto>().responseBody.blockLast()
+        val movie = webClient.get().uri("/api/v1/movie/get/${movieSavedFirstDummy?.id}").exchange()
+            .returnResult<MovieDto>().responseBody.blockLast()
+        val customerUpdated = webClient.post().uri("/api/v1/customer/${customer!!.id}/watched/add")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+            .bodyValue(movieSavedFirstDummy!!).exchange()
+            .returnResult<CustomerDto>().responseBody.blockLast()
+        assertThat(customerUpdated!!.watched).isNotNull.isNotEmpty.hasSize(1)
+        assertThat(customerUpdated.watched!![0].id).isEqualTo(movie!!.id)
+    }
+
+    @Test
+    fun `add to watched returns dto with correct favourites list`(){
+        val customer = webClient.get().uri("/api/v1/customer/get/${movieSavedFirstDummy?.id}").exchange()
+            .returnResult<CustomerDto>().responseBody.blockLast()
+        val movie = webClient.get().uri("/api/v1/movie/get/${movieSavedFirstDummy?.id}").exchange()
+            .returnResult<MovieDto>().responseBody.blockLast()
+        val customerUpdated = webClient.post().uri("/api/v1/customer/${customer!!.id}/favourites/add")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString())
+            .bodyValue(movieSavedFirstDummy!!).exchange()
+            .returnResult<CustomerDto>().responseBody.blockLast()
+        assertThat(customerUpdated!!.favourites).isNotNull.isNotEmpty.hasSize(1)
+        assertThat(customerUpdated.favourites!![0].id).isEqualTo(movie!!.id)
+    }
 
 }
